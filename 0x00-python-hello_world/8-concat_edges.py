bbc28503644cc9812855088d5b075efe4dee8985
Aziz <ngwk.zz@outlook.com>
2023-08-07 19:30:37 +0000

    The Zen of Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(str[39:-62] + "with Python")
-
+print(str[39:-63] + " with Python")
