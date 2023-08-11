ebd237bcb3668f496c68ca6beeeba5c708a085c8
Aziz <ngwk.zz@outlook.com>
2023-08-11 08:19:12 +0000

    CL args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+def print_args(args):
+    num_args = len(args)
+    if num_args == 0:
+        print(num_args, "arguments.")
+    elif num_args == 1:
+        print(num_args, "argument:")
+    elif num_args > 1:
+        print(num_args, "arguments:")
+
+    #for arg in args:
+     #   print(":", arg)
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+
+    print_args(arguments)
