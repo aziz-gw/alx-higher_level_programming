8924c672f37b6e94f401e1343c658ddfb2783705
Aziz <ngwk.zz@outlook.com>
2023-08-30 17:32:32 +0000

    Define a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Represents a square."""
+""" Represents a square."""
 
 
 class Square:
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Represents a square of size size"""
+
+
+class Square:
+    """ Defines a square with private instance attribute size """
+
+    def __init__(self, size=None):
+        self.__size = size
