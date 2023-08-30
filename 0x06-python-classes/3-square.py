a5e5a5282c2f1a45f76b74579056df5f238abbdf
Aziz <ngwk.zz@outlook.com>
2023-08-30 19:06:42 +0000

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" Represents a square """
+"""Represents a square"""
 
 
 class Square:
-    """ Defines a square with private instance attribute size """
+    """Defines a square with private instance attribute size."""
 
     def __init__(self, size=0):
         """
@@ -20,3 +20,9 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self):
+
+        """Return the current area of the square"""
+
+        return (self.__size * self.__size)
