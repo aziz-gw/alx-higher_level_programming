21ad63aa6f01a276ce8172985bb1e50077ea7776
Aziz <ngwk.zz@outlook.com>
2023-09-12 20:40:07 +0000

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Defines a class Student."""
+
+
+class Student:
+    """
+    Defines a class student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialises a new Student.
+
+        Args:
+            first_name (str): first name of a student.
+            last_name (str): last name of a student.
+            age (int): age of a student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student.
+        """
+        return self.__dict__
