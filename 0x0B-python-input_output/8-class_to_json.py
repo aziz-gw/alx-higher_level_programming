8addc92af97426de43107910ba3a4d9d4499b5ab
Aziz <ngwk.zz@outlook.com>
2023-09-12 20:34:00 +0000

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Defines the dictionary description with simple data 
-structure (list, dictionary, string, integer and boolean) 
-for JSON serialization of an object:
+Defines the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean) for JSON serialization
+of an object:
 """
 
 
