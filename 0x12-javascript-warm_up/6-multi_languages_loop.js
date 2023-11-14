64374a7ad91cdb33a8b094b19efb04f069c01986
Aziz <ngwk.zz@outlook.com>
2023-11-14 07:28:20 +0300

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function printStr () {
+  const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+  for (let i = 0; i <= 2; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printStr();
