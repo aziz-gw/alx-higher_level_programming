dbe84655a1840cf9b64dbd30dc47957f5a16b9ad
Aziz <ngwk.zz@outlook.com>
2023-12-02 08:22:25 +0300

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,11 +6,7 @@ the email as a parameter, and displays the body of the response
 (decoded in utf-8)
 """
 import urllib.request
-import urllib.parse
 import sys
-
-if len(sys.argv) < 3:
-    sys.exit(1)
 
 url = sys.argv[1]
 email = sys.argv[2]
