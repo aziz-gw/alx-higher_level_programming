267ca87f717cc9426e9b5013964159b03b70641b
Aziz <ngwk.zz@outlook.com>
2023-12-02 09:06:57 +0300

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes  in a URL, sends a request to the URL and displays the body of the
+response (decoded in utf-8).
+"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+try:
+    # Perform the request
+    with urllib.request.urlopen(url) as response:
+        # Read and decode the response body
+        response_body = response.read().decode('utf-8')
+        print(response_body)
+
+except urllib.error.HTTPError as e:
+    # Handle HTTP errors
+    print(f"Error code: {e.code}")
