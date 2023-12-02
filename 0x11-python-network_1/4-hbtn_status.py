020d4afe47cc9662c13e201f7fab042e4d40f817
Aziz <ngwk.zz@outlook.com>
2023-12-02 09:25:16 +0300

    Fetches url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+# Perform the request
+response = requests.get(url)
+
+# Display the response body with tabulation
+print(
+        f"Body response:\n"
+        f"\t- type: {type(response.text)}\n"
+        f"\t- content: {response.text}"
+)
