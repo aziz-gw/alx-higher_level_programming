6ac4f812e4a209057ca4121c519d070c5fe85b60
Aziz <ngwk.zz@outlook.com>
2023-12-02 15:39:29 +0300

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,14 +8,15 @@ the email as a parameter, and displays the body of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-# Encode the email parameter
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    # Encode the email parameter
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-# Perform the POST request
-with urllib.request.urlopen(url, data) as response:
-    # Read and decode the response body
-    response_body = response.read().decode('utf-8')
-    print(response_body)
+    # Perform the POST request
+    with urllib.request.urlopen(url, data) as response:
+        # Read and decode the response body
+        response_body = response.read().decode('utf-8')
+        print(response_body)
