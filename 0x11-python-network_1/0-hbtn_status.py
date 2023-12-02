2eb6fabd16d73f0d7e54018587406d98736b628e
Aziz <ngwk.zz@outlook.com>
2023-12-02 06:56:31 +0300

    Fetch status of a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    content_bytes = response.read()
+    content_str = content_bytes.decode('utf-8')
+
+print("Body response:")
+print("\t- type:", type(content_bytes))
+print("\t- content:", content_bytes)
+print("\t- utf8 content:", content)
