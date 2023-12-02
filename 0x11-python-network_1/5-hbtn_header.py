0e6a35fc5593e647ca2a6166d044df6bd393a2e4
Aziz <ngwk.zz@outlook.com>
2023-12-02 17:28:46 +0300

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,22 +6,10 @@ the variable X-Request-Id in the response header
 import requests
 import sys
 
-url = sys.argv[1]
-
-try:
+if __name__ == "__main__":
+    url = sys.argv[1]
     # Perform the request
     response = requests.get(url)
-    # Raise HTTPError for bad responses (4xx and 5xx)
-    response.raise_for_status()
     # Display the value of X-Request-Id in the response header
     request_id = response.headers.get('X-Request-Id')
-
-    if request_id:
-        print(f"{request_id}")
-
-except requests.exceptions.HTTPError as e:
-    # Handle HTTP errors
-    print(f"Error code: {e.response.status_code}")
-except requests.exceptions.RequestException as e:
-    # Handle other request-related errors
-    print(f"Request error: {e}")
+    print(f"{request_id}")
