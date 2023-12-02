25c7b9db1c8bcefdae926199fbb2cdbab6d48ddf
Aziz <ngwk.zz@outlook.com>
2023-12-02 17:44:12 +0300

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,23 +7,13 @@ body of the response.
 import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-# Prepare the data for the POST request
-data = {'email': email}
-
-try:
+    # Prepare the data for the POST request
+    data = {'email': email}
     # Perform the POST request
     response = requests.post(url, data=data)
-    # Raise HTTPError for bad responses (4xx and 5xx)
-    response.raise_for_status()
     # Display the response body
     print(f"{response.text}")
-
-except requests.exceptions.HTTPError as e:
-    # Handle HTTP errors
-    print(f"Error code: {e.response.status_code}")
-except requests.exceptions.RequestException as e:
-    # Handle other request-related errors
-    print(f"Request error: {e}")
