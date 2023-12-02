17ea9d396cf58bc5f105b2cc6972dad4de8990fa
Aziz <ngwk.zz@outlook.com>
2023-12-02 16:13:28 +0300

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,15 +6,14 @@ response (decoded in utf-8).
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
-try:
-    # Perform the request
-    with urllib.request.urlopen(url) as response:
-        # Read and decode the response body
-        response_body = response.read().decode('utf-8')
-        print(response_body)
-
-except urllib.error.HTTPError as e:
-    # Handle HTTP errors
-    print(f"Error code: {e.code}")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        # Perform the request
+        with urllib.request.urlopen(url) as response:
+            # Read and decode the response body
+            response_body = response.read().decode('utf-8')
+            print(response_body)
+    except urllib.error.HTTPError as e:
+        # Handle HTTP errors
+        print(f"Error code: {e.code}")
