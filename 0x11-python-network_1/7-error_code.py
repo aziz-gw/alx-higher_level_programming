d7ce59ffc80e00dc0d30157c3d4ce78f893b3ee2
Aziz <ngwk.zz@outlook.com>
2023-12-02 18:09:05 +0300

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,26 +3,22 @@
 Takes in a URL, sends a request to the URL and displays the body
 of the response.
 """
+
 import requests
 import sys
 
-url = sys.argv[1]
-
-try:
-    # Perform the GET request
-    response = requests.get(url)
-    # Raise HTTPError for bad responses (4xx and 5xx)
-    response.raise_for_status()
-    # Display the response body
-    print(f"{response.text}")
-
-    # Check for HTTP status code and print an error message if needed
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
-
-except requests.exceptions.HTTPError as e:
-    # Handle HTTP errors
-    print(f"Error code: {e.response.status_code}")
-except requests.exceptions.RequestException as e:
-    # Handle other request-related errors
-    print(f"Request error: {e}")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        # Perform the GET request
+        response = requests.get(url)
+        # Raise HTTPError for bad responses (4xx and 5xx)
+        response.raise_for_status()
+        # Display the response body
+        print(f"{response.text}")
+        # Check for HTTP status code and print an error message if needed
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+    except requests.exceptions.HTTPError as e:
+        # Handle HTTP errors
+        print(f"Error code: {e.response.status_code}")
