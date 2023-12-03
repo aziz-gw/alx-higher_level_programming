73f6b8d79efee364fb06fd822ab8d1a96af3f9dd
Aziz <ngwk.zz@outlook.com>
2023-12-03 18:02:40 +0300

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,14 +10,22 @@ import sys
 def get_user_id(username, token):
     url = "https://api.github.com/user"
     headers = {'Authorization': f'token {token}'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception if the request fails
         user_data = response.json()
         user_id = user_data.get('id')
         print(f"{user_id}")
+    except requests.RequestException as e:
+        print(f"Error fetching user data: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    token = sys.argv[2]
+    if len(sys.argv) != 3:
+        print("Usage: python github_user_id.py <username> <token>")
+        sys.exit(1)
+
+    username, token = sys.argv[1], sys.argv[2]
     get_user_id(username, token)
