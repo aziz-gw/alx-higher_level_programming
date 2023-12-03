642344457a4e6e334f68cbd39b3c03f913d892d0
Aziz <ngwk.zz@outlook.com>
2023-12-03 18:02:40 +0300

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,39 +3,21 @@
 Takes in GitHub credentials (username and password) and uses the GitHub API
 to display user id
 """
-
-import sys
 import requests
-from base64 import b64encode
+import sys
 
 
-def display_id(username, password):
-    """
-    Takes GitHub credentials and uses the GitHub API to display user id
-    """
+def get_user_id(username, token):
     url = "https://api.github.com/user"
-
-    # Encode the username and password for Basic Authentication
-    credentials = f"{username}:{password}"
-    headers = {"Authorization": "Basic " +
-               b64encode(credentials.encode()).decode("utf-8")}
-
+    headers = {'Authorization': f'token {token}'}
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
-        user_info = response.json()
-        user_id = user_info.get("id")
-        print(f"GitHub User ID: {user_id}")
+        user_data = response.json()
+        user_id = user_data.get('id')
+        print(f"{user_id}")
 
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <username> <password>")
-        sys.exit(1)
-
-    # Get GitHub credentials from command-line arguments
-    _username = sys.argv[1]
-    _password = sys.argv[2]
-
-    display_id(_username, _password)
+    username = sys.argv[1]
+    token = sys.argv[2]
+    get_user_id(username, token)
