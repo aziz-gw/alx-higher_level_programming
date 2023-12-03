8f8f2d978e5f36e6843c33ab482652316e2800ee
Aziz <ngwk.zz@outlook.com>
2023-12-03 20:00:49 +0300

    List 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Takes 2 arguments, repo name (rails) and user (rails) and lists 10 commits
+from the most recent to oldest
+"""
+import requests
+import sys
+
+
+def get_commits(repo_name, owner_name):
+    """
+    Takes 2 arguments and lists 10 commits from the most recent to oldest
+    """
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception if the request fails
+        commits = response.json()
+
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+
+    except requests.RequestException as e:
+        print(f"Error fetching commits: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    repo_name, owner_name = sys.argv[1], sys.argv[2]
+    get_commits(repo_name, owner_name)
